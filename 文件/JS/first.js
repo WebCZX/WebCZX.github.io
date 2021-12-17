cea12cf8df463e57391c78912058b1d5a5403c69
czx <1533266877@qq.com>
2021-12-17 19:35:48 +0800

    改进

--- 文件/JS/first.js
+++ 文件/JS/first.js
@@ -14,7 +14,7 @@ document.addEventListener('music', function () {
   audioAutoPlay();
 });
 //--创建触摸监听，当浏览器打开页面时，触摸屏幕触发事件，进行音频播放
-document.addEventListener('music', function () {
+document.addEventListener('touchstart', function () {
   function audioAutoPlay() {
     var audio = document.getElementById('music');
     audio.play();
