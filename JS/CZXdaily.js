50ba20e4a0879d87403608ca9ab079a98dddc6f3
czx <1533266877@qq.com>
2022-11-17 09:33:41 +0800

    add dirary 20221116

--- JS/CZXdaily.js
+++ JS/CZXdaily.js
@@ -2,4 +2,7 @@ $(function () {
   $.get('CZXdirary/1114.html', function (data) {
     $('#1114').html(data);
   });
+  $.get('CZXdirary/1116.html', function (data) {
+    $('#1116').html(data);
+  });
 });
